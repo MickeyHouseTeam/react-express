a78a4c796d78ffed293091d9cf4379a64a68d217
yaoqing1506440127 <44690882+yaoqing1506440127@users.noreply.github.com>
2020-04-01 19:12:18 +0800

    提交代码

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const path = require('path')
 const db = require('./db/connect')  //启动服务器的时候同时启动数据库
 const bodyParser = require('body-parser')
 const app = express()
+let userRouter=require('./routes/userRouter')
 
 //post 数据的解析 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 // 静态资源路径
 app.use('/public',express.static(path.join(__dirname,'./public')))
-
+app.use('/user',userRouter)
 
 app.listen(3000,()=>{
     console.log('服务端开启')
